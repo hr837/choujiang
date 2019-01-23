8967d05f0f9a2e7e20be18764455697c45fc42aa
huangrui <hr837@163.com>
2019-01-23 18:34:16 +0800

    修改logo文件地址

--- src/components/poker.vue
+++ src/components/poker.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="content">
         <div v-if="particularPoker" class="particularPoker">
-          <div class="poker-par" :style="{'background-image':'url(./logo.png)',color:'green'}" :class="`poker-par-${particularPokerStr}`"></div>
+          <div class="poker-par" :style="{backgroundImage: `url(${imgUrl})`}" :class="`poker-par-${particularPokerStr}`"></div>
         </div>
         <div v-else class="poker-type-list" :class="`item-count-${normalNumber}`">
           <div class="poker-type" :class="`poker-item-${item}`" v-for="item of normalNumber" :key="item">{{pokerType}}</div>
@@ -31,6 +31,9 @@ export default class Poker extends Vue {
 
   @Prop({ type: Boolean })
   private done!: boolean
+
+  // img url 需要使用变量引用地址，然后再模板使用该地址
+  private imgUrl = require("@/assets/logo.png")
 
   private get pokerNumber() {
     if (!this.pokerStr) return ""
